50e98f2f3e73b80146450f7941ab99822853d2fb
a-givertzman <a.givertzman@icloud.com>
2024-08-11 14:55:49 +0300

    AppSettings for database connection

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,25 @@
 import 'dart:async';
 
-// import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:firebase_core/firebase_core.dart';
 import 'package:ext_rw/ext_rw.dart';
 import 'package:flowers_app/domain/auth/app_user.dart';
 import 'package:flowers_app/domain/auth/authenticate.dart';
 import 'package:flowers_app/presentation/auth/sign_in/sign_in_page.dart';
 import 'package:flowers_app/presentation/core/app_theme.dart';
+import 'package:flowers_app/settings/app_settings.dart';
+import 'package:flowers_app/settings/setting.dart';
 import 'package:flutter/material.dart';
-import 'package:hmi_core/hmi_core.dart';
-import 'package:hmi_core/hmi_core_app_settings.dart';
+import 'package:hmi_core/hmi_core_failure.dart';
+import 'package:hmi_core/hmi_core_log.dart';
+import 'package:hmi_core/src/core/json/json_map.dart';
+import 'package:hmi_core/src/core/text_file.dart';
 ///
 /// Application entry point
 void main() {
+  Log.initialize(level: LogLevel.all);
+
   runZonedGuarded(
     () async {
-      // WidgetsFlutterBinding.ensureInitialized();
+      WidgetsFlutterBinding.ensureInitialized();
       // await Firebase.initializeApp();
       await _initStatics();      
       final AppUserSqlAccess clientSqlAccess = SqlAccess(
---
+++ lib/settings/app_settings.dart
@@ -0,0 +1,45 @@
+
+import 'package:hmi_core/hmi_core.dart';
+import 'package:hmi_core/hmi_core_result_new.dart';
+///
+///
+class AppSettings {
+  static const _log = Log('AppSettings');
+  static final _map = <String, dynamic>{
+    'displaySizeWidth': 1024,
+    'displaySizeHeight': 768,
+    // Place Durations in milliseconds!
+    'flushBarDurationLong': 8000,
+    'flushBarDurationMedium': 4000,
+    'flushBarDurationShort': 2000,
+    'smallPadding': 4.0,
+    'padding': 8.0,
+    'blockPadding': 16.0,
+    'floatingActionButtonSize': 60.0,
+    'floatingActionIconSize': 45.0,
+  };
+  ///
+  static Future<void> initialize({JsonMap<dynamic>? jsonMap}) async {
+    if (jsonMap != null) {
+      await jsonMap.decoded
+        .then((result) {
+          _log.warning('.initialize | Result: $result');
+          return switch(result) {
+            Ok(value: final map) => _map.addAll(map),
+            Err(: final error) => _log.warning('Failed to initialize app settings from file. Error: $error'),
+          };
+        },);
+    }
+  }
+  ///
+  static dynamic getSetting(String settingName) {
+    final setting = _map[settingName];
+    if (setting == null) {
+      throw Failure(
+        message: 'Ошибка в методе $AppSettings.getSetting(): Не найдена настройка "$settingName"',
+        stackTrace: StackTrace.current,
+      );
+    }
+    return setting;
+  }
+}
